b29f114a1208f8c20c1acbaa804cb4edb43e7cad
questoeunaccount <questaeunamail@protonmail.ch>
2021-06-21 18:07:01 +0200

    Update Challenge.R

--- Challenge.R
+++ Challenge.R
@@ -1,9 +1,24 @@
+# Cleaning the table ----
 rm(list = ls())
 mainDir <- getwd()
 Dir.Data <- file.path(mainDir, "Data")
 
+# Packages ----
+require(tidyverse)
+require(circlize)
+
+# df ----
 min_df <- read.csv(file = file.path(Dir.Data, "Interac_lower.csv"))[,-1]
 mean_df <- read.csv(file = file.path(Dir.Data, "Interac_mean.csv"))[,-1]
 max_df <- read.csv(file = file.path(Dir.Data, "Interac_upper.csv"))[,-1]
-
 uncertainty_df <- max_df - min_df
+
+# chord diagram ----
+min_df<-as.matrix(min_df)
+rownames(min_df) = letters[1:5]
+colnames(min_df) = letters[1:5]
+
+chordDiagram(min_df)
+chordDiagram(as.matrix(mean_df))
+chordDiagram(as.matrix(max_df))
+chordDiagram(as.matrix(uncertainty_df))
