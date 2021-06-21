fa94dee863a401d1529bd4d26b12437a67ef8e33
Erik Kusch (Tower PC) <erik@i-solution.de>
2021-06-21 16:59:03 +0200

    Fixes in data reading

--- Challenge.R
+++ Challenge.R
@@ -2,8 +2,8 @@ rm(list = ls())
 mainDir <- getwd()
 Dir.Data <- file.path(mainDir, "Data")
 
-min_df <- read.csv(file = file.path(Dir.Data, "Interac_lower.csv"))
-mean_df <- read.csv(file = file.path(Dir.Data, "Interac_mean.csv"))
-max_df <- read.csv(file = file.path(Dir.Data, "Interac_upper.csv"))
+min_df <- read.csv(file = file.path(Dir.Data, "Interac_lower.csv"))[,-1]
+mean_df <- read.csv(file = file.path(Dir.Data, "Interac_mean.csv"))[,-1]
+max_df <- read.csv(file = file.path(Dir.Data, "Interac_upper.csv"))[,-1]
 
 uncertainty_df <- max_df - min_df
